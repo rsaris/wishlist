f420c0a8b50a9b3a64ad152f66a833a9c221403e
Bob Saris <bob@mobilesuitesapp.com>
2015-09-08 18:52:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishlist::Application.config.secret_key_base = '04fe9a2830823c3d32e6d7f88f7dbb2286559367646fba798c7c463d76de5afcd9584b1762c8e62f34424ffe0fc6cd1a6053e366bcc79f50e488b99679311348'
