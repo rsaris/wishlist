8ec8504768c121044ced0901e1dfe58e43b99829
Bob Saris <bob@mobilesuitesapp.com>
2015-11-08 23:44:35 -0500

    fixed bug where we destroyed the 'buyer' when we destroyed a gift

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,6 +1,6 @@
 class Gift < ActiveRecord::Base
   belongs_to :user
-  belongs_to :buyer, foreign_key: :buyer_user_id, inverse_of: :gifts_purchased, class_name: 'User', dependent: :destroy
+  belongs_to :buyer, foreign_key: :buyer_user_id, inverse_of: :gifts_purchased, class_name: 'User'
 
   validates :user, presence: true
   validates :buyer, presence: true
